0f06ac9533cb27778b0d9730ade55a4904e865b5
Valeria <valeria.casatti@gmail.com>
2025-05-28 13:52:37 -0300

    email sending configuration with nodemailer

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { config } from "./config/config.js";
+import { transporter } from "./config/gmail.js";
 
 // server
 const PORT = config.server.port;
@@ -11,3 +12,23 @@ app.listen(PORT, () => {
 
 app.use(cors());
 app.use(express.json());
+
+app.post("/api/contact", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  try {
+    const emailOptions = {
+      from: email,
+      to: config.gmail.account,
+      subject: `Nuevo mensaje desde tu portfolio de parte de ${name}`,
+      html: `<p>${message}</p>`,
+    };
+
+    await transporter.sendMail(emailOptions);
+
+    res.status(200).send("Email sent successfully");
+  } catch (e) {
+    console.log(`Error sending email: ${e.message}`);
+    res.status(500).send("Error sending email");
+  }
+});
