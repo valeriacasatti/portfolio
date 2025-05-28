309a63de227a11dadaeb59c2673d76d713295978
Valeria <valeria.casatti@gmail.com>
2025-05-28 14:18:38 -0300

    fix: remove old public/src and move frontend correctly

--- src/components/context/useTheme.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import ThemeContext from "./ThemeContext";
-
-export const useTheme = () => {
-  const context = useContext(ThemeContext);
-  return context;
-};
