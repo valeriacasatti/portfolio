b983d8255adbc2d49c4b4b75606c51ccf3756804
Valeria <valeria.casatti@gmail.com>
2025-05-28 17:34:12 -0300

    error 404 page

--- frontend/src/components/pages/error404/Error404.jsx
+++ frontend/src/components/pages/error404/Error404.jsx
@@ -1,4 +1,29 @@
+import error404 from "../../../../public/404.png";
+import { Box } from "@mui/material";
+import { useTheme } from "../../context/useTheme";
+import Button from "../../common/button/Button";
+import { Link } from "react-router-dom";
+
 const Error404 = () => {
-  return <div>Error404</div>;
+  const { theme } = useTheme();
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: theme.palette.background.default,
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: "2rem",
+      }}
+    >
+      <img src={error404} alt="error-404" />
+      <Button className="contained-btn">
+        <Link to="/">go to home page</Link>
+      </Button>
+    </Box>
+  );
 };
 export default Error404;
